55fb4a0b43ea14b62ed88b8599b26a98105a4a25
Atlan <gevaliatlaniskt@gmail.com>
2023-07-07 00:00:03 +0200

    branched project in FE and BE

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+{{ count.toFixed(2) }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(1)
+
+</script>
+
+<style scoped>
+  
+</style>
